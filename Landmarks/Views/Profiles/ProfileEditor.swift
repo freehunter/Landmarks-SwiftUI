0e533164730b353b261511688d289b7226393226
Mark Hunt <thefreehunter@gmail.com>
2023-12-23 13:05:51 -0500

    adding ability to edit user profile and set a goal date

---
+++ Landmarks/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by Mark Hunt on 12/23/23.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditor()
+}
