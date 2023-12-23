cfac438849fab9653684d9078e24704a9889ed7d
Mark Hunt <thefreehunter@gmail.com>
2023-12-23 11:48:31 -0500

    working on featured page

---
+++ Landmarks/Views/CategoryItem.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by Mark Hunt on 12/23/23.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryItem()
+}
